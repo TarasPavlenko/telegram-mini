778618e7e244c4166a2012e54d4907bc47a78902
Taras Pavlenko <taras.pavlenko358@gmail.com>
2025-11-19 01:04:32 +0200

    remove default pages

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,43 +1,39 @@
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import { Button, FixedLayout, Placeholder } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
-import { Link } from '@/components/Link/Link.tsx';
 import { Page } from '@/components/Page.tsx';
 
-import tonSvg from './ton.svg';
-
 export const IndexPage: FC = () => {
-  return (
-    <Page back={false}>
-      <List>
-        <Section
-          header="Features"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-        >
-          <Link to="/ton-connect">
-            <Cell
-              before={<Image src={tonSvg} style={{ backgroundColor: '#007AFF' }}/>}
-              subtitle="Connect your TON wallet"
+    return (
+        <Page back={false}>
+            <Placeholder
+                header="Collector 3000"
+                style={{
+                    fontSize: 100
+                }}
             >
-              TON Connect
-            </Cell>
-          </Link>
-        </Section>
-        <Section
-          header="Application Launch Data"
-          footer="These pages help developer to learn more about current launch information"
-        >
-          <Link to="/init-data">
-            <Cell subtitle="User data, chat information, technical data">Init Data</Cell>
-          </Link>
-          <Link to="/launch-params">
-            <Cell subtitle="Platform identifier, Mini Apps version, etc.">Launch Parameters</Cell>
-          </Link>
-          <Link to="/theme-params">
-            <Cell subtitle="Telegram application palette information">Theme Parameters</Cell>
-          </Link>
-        </Section>
-      </List>
-    </Page>
-  );
+                💰
+            </Placeholder>
+            <FixedLayout style={{
+                display: "flex",
+                padding: 16,
+                gap: 16
+            }}>
+                <Button
+                    mode="bezeled"
+                    size="l"
+                    stretched
+                >
+                    Check
+                </Button>
+                <Button
+                    mode="bezeled"
+                    size="l"
+                    stretched
+                >
+                    Collect
+                </Button>
+            </FixedLayout>
+        </Page>
+    );
 };
